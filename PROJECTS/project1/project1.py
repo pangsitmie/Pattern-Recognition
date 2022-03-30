5cd2335b18bdaf228f87ed309535d8ad13f7ad99
pangsitmie <pangsitmie@github.com>
2022-03-30 11:00:36 +0800

    update normal distribution curve

--- PROJECTS/project1/project1.py
+++ PROJECTS/project1/project1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from random import random
 import matplotlib.pyplot as plt
+import collections
 
 
 # d = number of flips
@@ -23,6 +24,18 @@ def generate_flip(prob, d):
             List2 = tails.append(1)
     return (len(heads))
 
+def pdf(n,theta,x):
+    mean = n*theta
+    std = n * theta * (1- theta)
+    # mean = np.mean(x)
+    # std = np.std(x)
+    y_list = []
+    for i in range(x):
+        y_list.append(1/(std * np.sqrt(2 * np.pi)) * np.exp( - (x - mean)**2 / (2 * std**2)))
+    # y_out = 1/(std * np.sqrt(2 * np.pi)) * np.exp( - (x - mean)**2 / (2 * std**2))
+    print(mean, std,"...")
+    return y_list
+
 
 
 #initialize variables
@@ -33,7 +46,7 @@ d=100
 n = 10000
 
 #case1
-theta1 = 3/5
+theta1 = 1/2
 print("#(head, tails) for", d, "flips and", n ,"tries with theta=" ,theta1)
 for times in range (0,n):
         z = generate_flip(theta1,d)
@@ -65,18 +78,59 @@ print("The sum of the probabilities is:", sum, '\n')
 
 
 
+od1 = collections.OrderedDict(sorted(dict1.items()))
+print(od1)
 
-lists = sorted(dict1.items()) # sorted by key, return a list of tuples
+od2 = collections.OrderedDict(sorted(dict2.items()))
+print(od2)
+
+
+lists = sorted(od1.items()) # sorted by key, return a list of tuples
 x, y = zip(*lists) # unpack a list of pairs into two tuples
 
-lists = sorted(dict2.items()) # sorted by key, return a list of tuples
+print(x)
+
+lists = sorted(od2.items()) # sorted by key, return a list of tuples
 v, w = zip(*lists) # unpack a list of pairs into two tuples
 
-plt.bar(x,y)#0.5
-plt.plot(x,y)#0.5
+
+# plt.figure(figsize = (6, 6))
+
+#plot data
+
+output1 = list(od1.values())
+probability1 = [i / n for i in output1]
+print("all values in dict1 are:", probability1)
+
+output2 = list(od2.values())
+probability2 = [i / n for i in output2]
+print("all values in dict2 are:", probability2)
+
+
+# plt.bar(x,probability1)#0.5
+# plt.plot(x,probability1)#0.5
+
+print(output1)
+
+print("mean:", np.mean(output1))
+# PLOT THE GAUSIAN CURVE BASED ON PDF
+gaus1 = pdf(n,theta1,probability1)
+print("gaus1: ",gaus1)
+# plt.plot(x, gaus1, color = 'black', linestyle = 'dashed')
+# plt.scatter( x, gaus1, marker = 'o', s = 25, color = 'red')
+
+
+
+# plt.bar(v,w)#0.5
+# plt.plot(v,w)#0.5
+
+
 
 plt.title(f'Histogram of {d} flips, {n} tries, and Theta = {theta1} & {theta2}')
 plt.xlabel("X Number of heads")
-plt.ylabel("Frequency")
+plt.ylabel("Probability")
 plt.legend([f'Theta = {theta1}', f'Theta = {theta2}'])
+
+
 plt.show()
+
